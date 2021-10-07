5af7b0bf4c2e65f35e625c24db02f0a8b551cf2f
dhef415 <dhef415@gmail.com>
2021-10-07 14:40:31 -0700

    complete

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -44,19 +44,26 @@ async function findSteps(scheme_id) {
 }
 
 function add(scheme) {
-  return db('schemes').insert(scheme)
-    .then([id]) => {
-      return db('schemes').where('id, id')
-    }
+  return db('schemes')
+    .insert(scheme)
+    .then(([id]) => {
+      return db('schemes').where('scheme_id', id).first()
+    })
 }
 
 function addStep(scheme_id, step) {
-  // EXERCISE E
-  /*
-    1E- This function adds a step to the scheme with the given `scheme_id`
-    and resolves to _all the steps_ belonging to the given `scheme_id`,
-    including the newly created one.
-  */
+  return db('steps')
+    .insert({
+      ...step,
+      scheme_id,
+    })
+    .then(() => {
+      return db('steps as st')
+        .join('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
+        .select('step_id', 'step_number', 'instructions', 'scheme_name')
+        .orderBy('step_number')
+        .where('sc.scheme_id', scheme_id)
+    })
 }
 
 module.exports = {
